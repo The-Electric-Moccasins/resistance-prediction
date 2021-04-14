b26f45b5e11f1f5049156de68c81face8d175d4f
Noam <>
2021-04-14 14:59:03 +0000

    dataset pipeline

--- data_pipeline.py
+++ data_pipeline.py
@@ -9,9 +9,10 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
 params = HyperParams()
-# cohort_query =  query_esbl_pts
-cohort_query =  query_all_pts
-RECALC_LOINC_VALS = False
+print(params.__dict__)
+# cohort_query =  lambda: query_esbl_pts()
+cohort_query =  lambda: query_all_pts(params.observation_window_hours)
+RECALC_LOINC_VALS = True
 DATA_DIR = 'data_ae_train'  # 'data'
 data_file_name = f'{DATA_DIR}/fulldata.npy'
 
@@ -26,8 +27,11 @@ def main():
     # Remove dups
     esbl_admits = remove_dups(esbl_admits)
     print("removed dups")
+    print(f"esbl_admits: {esbl_admits.shape}")
+    print(esbl_admits.head())
     # Create observation window
     esbl_admits_window = observation_window(esbl_admits, window_size=params.observation_window_hours)
+    print(f"esbl_admits_window: {esbl_admits_window.shape}")
     # Subset columns
     pts_labels = esbl_admits_window[['hadm_id', 'index_date','RESISTANT_YN']]
     pts_labels.to_pickle(f'{DATA_DIR}/patient_labels.pkl')
@@ -40,14 +44,24 @@ def main():
     # Patient's features data:
 
     # Loading the features
+    
     features = dataset_creation(pts_labels['hadm_id'], params.observation_window_hours)
     features = features.merge(pts_labels[['hadm_id','RESISTANT_YN']], on='hadm_id')
     features.to_pickle(f'{DATA_DIR}/features.pkl')
+    
+    # Make sure we have the same columns as in the cohort
+    cols = ['hadm_id', 'subject_id', '10378-8', '10535-3', '10839-9', '11555-0', '11556-8', '11557-6', '11558-4', '13362-9', '1644-4', '1742-6', '1751-7', '17849-1', '1798-8', '1863-0', '1920-8', '1959-6', '1963-8', '1968-7', '1971-1', '1975-2', '1988-5', '1994-3', '19991-9', '19994-3', '2000-8', '20077-4', '20112-9', '20564-1', '20569-0', '20570-8', '20578-1', '2069-3', '2075-0', '2078-4', '2085-9', '2090-9', '2093-3', '2143-6', '2157-6', '2160-0', '2161-8', '2170-9', '2276-4', '2284-8', '2339-0', '2345-7', '2498-4', '2500-7', '2532-0', '2601-3', '26498-6', '2692-2', '2695-5', '2777-1', '2823-3', '2828-2', '28541-1', '2947-0', '2951-2', '2955-3', '30089-7', '3016-3', '30226-5', '3040-3', '3094-0', '3095-7', '3151-8', '3173-2', '3255-7', '32693-4', '3297-9', '3349-8', '3376-1', '33762-6', '3377-9', '3390-2', '33914-3', '3397-7', '34728-6', '3773-9', '3879-4', '3967-7', '4023-8', '4073-3', '42662-7', '4542-7', '4544-3', '4548-4', '48065-7', '5642-4', '5767-9', '5769-5', '5770-3', '5778-6', '5787-7', '5792-7', '5793-5', '5794-3', '5796-8', '5797-6', '5799-2', '5802-4', '5803-2', '5804-0', '5808-1', '5811-5', '5818-0', '5821-4', '5822-2', '5895-7', '5902-2', '6298-4', '6598-7', '6768-6', '6773-6', '702-1', '704-7', '711-2', '718-7', '728-6', '731-0', '733-6', '738-5', '741-9', '742-7', '761-7', '763-3', '772-4', '774-0', '777-3', '778-1', '779-9', '7790-9', '7791-7', '785-6', '786-4', '787-2', '788-0', '789-8', '800-3', '804-5', '8246-1', '8247-9', '9322-9', 'admittime', 'admission_type', 'admission_location', 'insurance', 'language', 'religion', 'marital_status', 'ethnicity', 'gender', 'RESISTANT_YN']
+    
+    features = features[cols]
+
 
     # features = pd.read_pickle(f'{DATA_DIR}/features.pkl')
 
 
-    loinc_codes = list(features.drop(columns=['hadm_id', 'subject_id', 'admittime','admission_type']).columns)[:-8]
+#     loinc_codes = list(features.drop(columns=['hadm_id', 'subject_id', 'admittime','admission_type']).columns)[:-8]
+
+    # make sure we have the same loinc codes as in the cohort
+    loinc_codes = ['10378-8', '10535-3', '10839-9', '11555-0', '11556-8', '11557-6', '11558-4', '13362-9', '1644-4', '1742-6', '1751-7', '17849-1', '1798-8', '1863-0', '1920-8', '1959-6', '1963-8', '1968-7', '1971-1', '1975-2', '1988-5', '1994-3', '19991-9', '19994-3', '2000-8', '20077-4', '20112-9', '20564-1', '20569-0', '20570-8', '20578-1', '2069-3', '2075-0', '2078-4', '2085-9', '2090-9', '2093-3', '2143-6', '2157-6', '2160-0', '2161-8', '2170-9', '2276-4', '2284-8', '2339-0', '2345-7', '2498-4', '2500-7', '2532-0', '2601-3', '26498-6', '2692-2', '2695-5', '2777-1', '2823-3', '2828-2', '28541-1', '2947-0', '2951-2', '2955-3', '30089-7', '3016-3', '30226-5', '3040-3', '3094-0', '3095-7', '3151-8', '3173-2', '3255-7', '32693-4', '3297-9', '3349-8', '3376-1', '33762-6', '3377-9', '3390-2', '33914-3', '3397-7', '34728-6', '3773-9', '3879-4', '3967-7', '4023-8', '4073-3', '42662-7', '4542-7', '4544-3', '4548-4', '48065-7', '5642-4', '5767-9', '5769-5', '5770-3', '5778-6', '5787-7', '5792-7', '5793-5', '5794-3', '5796-8', '5797-6', '5799-2', '5802-4', '5803-2', '5804-0', '5808-1', '5811-5', '5818-0', '5821-4', '5822-2', '5895-7', '5902-2', '6298-4', '6598-7', '6768-6', '6773-6', '702-1', '704-7', '711-2', '718-7', '728-6', '731-0', '733-6', '738-5', '741-9', '742-7', '761-7', '763-3', '772-4', '774-0', '777-3', '778-1', '779-9', '7790-9', '7791-7', '785-6', '786-4', '787-2', '788-0', '789-8', '800-3', '804-5', '8246-1', '8247-9', '9322-9']
     # print(list(loinc_codes))
 
     features_summary = features[loinc_codes].describe()
@@ -272,3 +286,4 @@ def main():
 
 
     np.save(data_file_name, fulldata)
+    print(f"done, saved to {data_file_name}")
